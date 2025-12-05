be4ef43fe8c8bc6403605b625b9a5f34e98b347a
sltechytsl-alt <banukaweerapperuma@gmail.com>
2025-12-05 06:40:51 +0530

    add three files

--- my-app/src/components/three/ModelSwitcher.jsx
+++ my-app/src/components/three/ModelSwitcher.jsx
@@ -5,7 +5,6 @@ import gsap from 'gsap';
 import MacbookModel16 from "../models/Macbook-16.jsx";
 import MacbookModel14 from "../models/Macbook-14.jsx";
 import {useGSAP} from "@gsap/react";
-
 const ANIMATION_DURATION = 1;
 const OFFSET_DISTANCE = 5;
 
